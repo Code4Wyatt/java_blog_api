5df7de1774b3c197dadd012ecd7bf1e294fea69d
Paul <pajmurray@outlook.com>
2023-03-12 15:48:41 +0000

    add register rest api

--- src/main/java/com/springboot/blog/service/impl/AuthServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/AuthServiceImpl.java
@@ -1,20 +1,40 @@
 package com.springboot.blog.service.impl;
 
+import com.springboot.blog.exception.BlogApiException;
+import com.springboot.blog.model.Role;
+import com.springboot.blog.model.User;
 import com.springboot.blog.payload.LoginDto;
+import com.springboot.blog.payload.RegisterDto;
+import com.springboot.blog.repository.RoleRepository;
+import com.springboot.blog.repository.UserRepository;
 import com.springboot.blog.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
     private AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public AuthServiceImpl(AuthenticationManager authenticationManager) {
+    public AuthServiceImpl(AuthenticationManager authenticationManager,
+                           UserRepository userRepository,
+                           RoleRepository roleRepository,
+                           PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -28,5 +48,33 @@ public class AuthServiceImpl implements AuthService {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         return "User logged in successfully.";
+    }
+
+    @Override
+    public String register(RegisterDto registerDto) {
+        // Add check that username exists in database
+        if(userRepository.existsByUsername(registerDto.getUsername())) {
+            throw new BlogApiException(HttpStatus.BAD_REQUEST, "Username already exists, please choose another.");
+        }
+
+        // Add check if email already exists in database
+        if(userRepository.existsByEmail(registerDto.getEmail())) {
+            throw new BlogApiException(HttpStatus.BAD_REQUEST, "An account with this email is already registered, please login.");
+        }
+
+        User user = new User();
+        user.setName(registerDto.getName());
+        user.setUsername(registerDto.getUsername());
+        user.setEmail(registerDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+        Role userRole = roleRepository.findByName("ROLE_USER").get();
+        roles.add(userRole);
+        user.setRoles(roles);
+
+        userRepository.save(user);
+
+        return "User registered successfully.";
     }
 }
